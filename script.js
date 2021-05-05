768eaba446398ce8930ac38f472f00aec3a77839
charliepoker <iheanachocharlie@gmail.com>
2021-05-05 19:15:23 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const joke = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokebtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  fetch('https://icanhazdadjoke.com', config)
+    .then(res => res.json())
+    .then(data => {
+      joke.innerHTML = data.joke;
+    });
+}
